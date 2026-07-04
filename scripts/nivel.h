962ea1ce9a70316c2e65524af8e2dcc21d22fbee
castillolutber-gif <castillolutber@gmail.com>
2026-07-03 19:32:44 -0500

    Delete scripts/nivel.h

--- scripts/nivel.h
+++
@@ -1,9 +0,0 @@
-#include <string>
-#include "../entidades/Escenario.h"
-
-class Nivel {
-public:
-    bool      validarRegla(const std::string& accion);
-    Escenario cargarEscenario(int id);
-    bool      esCompletado(const Escenario& esc);
-};
